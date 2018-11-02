03c8cad5660999796093b1fdead83437558cbdc0
Jason Vranek <jvranek@ucsc.edu>
2018-11-01 23:22:04 -0700

    refactored codebase

--- flaskr/flask_flow.py
+++ flaskr/flask_flow.py
@@ -1,9 +1,26 @@
+import sys
+sys.path.append('../flow/')
+
+from trader import Trader
+from order_book import OrderBook
+from exchange import Exchange
+from graph import Graph
+from simulation import random_html_graph
+
 from flask import Flask
+import random
+import numpy as np
+import time
+
+from profiler import prof
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	html = random_html_graph(50)
+	return html
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
